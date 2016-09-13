afdf7950fd50ec4358cbaf41d6d0fde2d6a453ee
Royston <rlqhenson@hotmail.com>
2016-09-13 14:04:01 +0100

    added directory.rb

---
+++ directory.rb
@@ -0,0 +1,14 @@
+puts """
+The students of Villains Academy
+--------------------
+Dr. Hannibal Lecter
+Darth Vader
+Nurse Ratched
+Michael Corleone
+Alex DeLarge
+The Wicked Witch of the West
+Terminator
+Freddy Krueger
+The Joker
+Norman Bates
+"""
