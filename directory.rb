c85b202fd77f53bc4af659bfa56ec90e6c479406
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-23 23:57:53 +0100

    Add student's cohort month to information

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # first we put the students into an array
 students = [
-    'Dr. Hannibal Lecter',
-    'Darth Vader',
-    'Nurse Ratched',
-    'Michael Corleone',
-    'Alex DeLarge',
-    'The Wicked Witch of the West',
-    'Terminator',
-    'Freddy Krueger',
-    'The Joker',
-    'Joffrey Baratheon',
-    'Norman Bates'
+    {name: 'Dr. Hannibal Lecter', cohort: :november},
+    {name: 'Darth Vader', cohort: :november},
+    {name: 'Nurse Ratched', cohort: :november},
+    {name: 'Michael Corleone', cohort: :november},
+    {name: 'Alex DeLarge', cohort: :november},
+    {name: 'The Wicked Witch of the West', cohort: :november},
+    {name: 'Terminator', cohort: :november},
+    {name: 'Freddy Krueger', cohort: :november},
+    {name: 'The Joker', cohort: :november},
+    {name: 'Joffrey Baratheon', cohort: :november},
+    {name: 'Norman Bates', cohort: :novembe}
 ]
 
 # and then print them
@@ -20,13 +20,13 @@ def print_header
 end
 
 
-def print(names)
-  names.each { |s| puts s }
+def print(students)
+  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
  # finally we print the total number of students
- def print_footer(names)
-  puts "\nOverall, we have #{names.size} great students!"
+ def print_footer(students)
+  puts "\nOverall, we have #{students.size} great students!"
  end
 
  print_header
