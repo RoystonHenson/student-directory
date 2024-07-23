17b81d858677aadfee99f8de967bdd08161c0411
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-23 20:08:31 +0100

    Print list of students

---
+++ directory.rb
@@ -0,0 +1,35 @@
+=begin
+puts 'The students of Villains Academy'
+puts '-' * 13
+puts 'Dr. Hannibal Lecter'
+puts 'Darth Vader'
+puts 'Nurse Ratched'
+puts 'Michael Corleone'
+puts 'Alex DeLarge'
+puts 'The Wicked Witch of the West'
+puts 'Terminator'
+puts 'Freddy Krueger'
+puts 'The Joker'
+puts 'Joffrey Baratheon'
+puts 'Norman Bates'
+=end
+
+puts <<-EOF
+
+    The Students of Villains Academy
+    #{'-' * 32}
+
+    Dr. Hannibal Lecter
+    Darth Vader
+    Nurse Ratched
+    Michael Corleone
+    Alex DeLarge
+    The Wicked Witch of the West
+    Terminator
+    Freddy Krueger
+    The Joker
+    Joffrey Baratheon
+    Norman Bates
+
+    EOF
+
