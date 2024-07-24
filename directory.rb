ee906cc97aa7edc3e17c930ab0f29b6fc27f4412
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-24 17:01:55 +0100

    Refactor to ask user to input students interactively

--- directory.rb
+++ directory.rb
@@ -1,17 +1,25 @@
-# first we put the students into an array
-students = [
-    {name: 'Dr. Hannibal Lecter', cohort: :november},
-    {name: 'Darth Vader', cohort: :november},
-    {name: 'Nurse Ratched', cohort: :november},
-    {name: 'Michael Corleone', cohort: :november},
-    {name: 'Alex DeLarge', cohort: :november},
-    {name: 'The Wicked Witch of the West', cohort: :november},
-    {name: 'Terminator', cohort: :november},
-    {name: 'Freddy Krueger', cohort: :november},
-    {name: 'The Joker', cohort: :november},
-    {name: 'Joffrey Baratheon', cohort: :november},
-    {name: 'Norman Bates', cohort: :novembe}
-]
+def input_students
+  puts "Please enter the names of the students\nTo finish, just hit return twice"
+  
+  # create an empty array
+  students = []
+ 
+  # get the first name
+  name = gets.chomp
+
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.size} students"
+
+    # get another name from the user
+    name = $stdin.gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 # and then print them
 def print_header
@@ -29,6 +37,7 @@ end
   puts "\nOverall, we have #{students.size} great students!"
  end
 
+ students = input_students
  print_header
  print(students)
  print_footer(students)
