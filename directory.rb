90fa8a1eed88963c296a4b41c68437c5343238ee
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-27 16:49:55 +0100

    Refactor methods

--- directory.rb
+++ directory.rb
@@ -1,85 +1,83 @@
 def menu
-  while true
-    puts "\nWelcome to the Student Directory. Please enter one of the folowing commands:"
-    puts """   
-             New   -  Add new students
-             Add   -  Add details for existing students
-             Show  -  Display list of existing students
-             Exit  -  Exit Student Directory
-         """
+  loop do
+    menu_introduction
     choice = $stdin.gets.chomp.downcase
-    if choice == 'new'
-      input_students
-    elsif choice == 'add'
-      add_detail
-    elsif choice == 'show'
-      if @students.nil? || @students.size < 1
-        puts 'There are no students in the directory!'
-      else
-        puts 'There are no students in the directory!' 
-        puts 'How would you like to display the students?:'
-        puts """
-             All     -  Lists all students
-             Month   -  You want to search students by month
-             Letter  -  You want to search students by first 
-                        letter of their name
-        """
-        choice = $stdin.gets.chomp.downcase
-        if choice == 'all'
-          print(@students)
-        elsif choice == 'month'
-          puts 'Enter the month of the cohort:'
-          choice = $stdin.gets.chomp.downcase.to_sym
-            if @students.any? { |student| student[:cohort] == choice }
-              print(@students.select { |student| student[:cohort] === choice })
-            else
-              puts 'Sorry, there are no students for that month.'
-            end
-        elsif choice == 'letter'
-          puts 'Please enter the first letter of the student\'s name'
-          choice = $stdin.gets.chomp.upcase
-          if @students.any? { |student| student[:name][0] == choice }
-            print(@students, choice)
-          else
-            puts 'Sorry, there are no students matching that search request.'
-          end
-        end
-      end
-    elsif choice == 'exit'
-      exit(0)
+    case choice
+    when 'new'  then input_students
+    when 'add'  then add_student_detail
+    when 'show' then puts @students.nil? ? 'There are no students in the directory!' : show_students
+    when 'exit' then exit(0)    
+    else puts 'Please enter one of the commands to continue:'
     end
   end
 end
 
 def input_students
-  # create an empty array
   @students = []
-  # get student name and cohort from user
-    name, cohort = 'ready', 'set'
-    while !name.empty? || !cohort.empty?
-      puts "Please enter the name of the student you want to add (Hit enter to cancel):"
-      name = $stdin.gets.chomp
-      break if name == ''
-      puts "What cohort is this student in? (Hit enter to cancel):"
-      cohort = $stdin.gets.chomp
-      break if cohort == ''
-      if !name.empty? && !cohort.empty?
-        # make sure student names are capitalised
-        name = capitalise_words(name) 
-        # add student hash to the array
-        @students << {name: name, cohort: cohort.to_sym}
-          puts "Now we have #{@students.size} #{singular_or_plural('student', @students.size)}."
-      end
-    end
-  # sort the students by cohort
+  name, cohort = 'ready', 'set'
+  while !name.empty? || !cohort.empty?
+    puts "Please enter the name of the student you want to add (Hit enter to cancel):"
+    name = $stdin.gets.chomp
+    name = capitalise_words(name) 
+    break if name == ''
+    puts "What cohort is this student in? (Hit enter to cancel):"
+    cohort = $stdin.gets.chomp
+    break if cohort == ''
+    @students << {name: name, cohort: cohort.to_sym} if !name.empty? && !cohort.empty?
+    puts "Now we have #{@students.size} #{singular_or_plural('student', @students.size)}."
+  end
   @students = @students.sort_by { |student| student[:cohort] } 
-  # return array of students
-  print_header
-  print(@students)
-  print_footer(@students)
+  display_students_list(@students)
 end
 
-def add_detail
+private
+
+def menu_introduction
+  puts "\nWelcome to the Student Directory. Please enter one of the following commands:"
+  puts """   
+             New   -  Add new students
+             Add   -  Add details for existing students
+             Show  -  Display list of existing students
+             Exit  -  Exit Student Directory
+       """
+end
+
+def print_header
+  puts "The Students of Villains Academy".center(80)
+  puts ('-' * 32).center(80)
+end
+
+def print(students, letter=false)
+  if letter
+    display_students(students.select { |student| student[:name][0].upcase == letter.upcase })
+  else
+    display_students(students)
+  end
+end
+
+def print_footer(students)
+  puts "\nOverall, we have #{@students.size} great #{singular_or_plural('student', @students.size)}! "\
+       'Note: Names over 12 characters long are currently not being shown.'.center(80)
+end
+
+def display_students(students)
+  count = 1
+  line_width = 30
+  while count <= students.size
+    if students[count - 1][:name].size < 12
+      puts "#{count}. #{students[count - 1][:name]}".rjust(line_width) + "(#{students[count - 1][:cohort].capitalize} cohort)".rjust(line_width)
+    end
+      count += 1
+    end
+end
+
+def display_students_list(students)
+  print_header
+  print(students)
+  print_footer(students)
+end
+
+def add_student_detail
   puts 'Enter the name of the student you would like to add information for:'
   name = $stdin.gets.chomp
   # check if student is in the directory
@@ -98,35 +96,42 @@ def add_detail
   end
 end
 
-# and then print them
-def print_header
-  puts "The Students of Villains Academy".center(80)
-  puts ('-' * 32).center(80)
+def submenu_show
+  puts 'How would you like to display the students?:'
+  puts """
+             All     -  Lists all students
+             Month   -  You want to search students by month
+             Letter  -  You want to search students by first 
+                        letter of their name
+       """
 end
 
-def print(students, letter=false)
-  if letter
-    display_students(students.select { |student| student[:name][0].upcase == letter.upcase })
-  else
-    display_students(students)
+def show_students
+  submenu_show
+  choice = $stdin.gets.chomp.downcase
+  if choice == 'all'
+    print(@students)
+  elsif choice == 'month'
+    show_students_by_month
+  elsif choice == 'letter'
+    puts 'Please enter the first letter of the student\'s name'
+    choice = $stdin.gets.chomp.upcase
+    if @students.any? { |student| student[:name][0] == choice }
+      print(@students, choice)
+    else
+      puts 'Sorry, there are no students matching that search request.'
+    end
   end
 end
 
-def display_students(students)
-  count = 1
-  line_width = 30
-  while count <= students.size
-    if students[count - 1][:name].size < 12
-      puts "#{count}. #{students[count - 1][:name]}".rjust(line_width) + "(#{students[count - 1][:cohort].capitalize} cohort)".rjust(line_width)
-    end
-      count += 1
-    end
-end
-
- # finally we print the total number of students
-def print_footer(students)
-  puts "\nOverall, we have #{@students.size} great #{singular_or_plural('student', @students.size)}! "\
-       'Note: Names over 12 characters long are currently not being shown.'.center(80)
+def show_students_by_month
+  puts 'Enter the month of the cohort:'
+  choice = $stdin.gets.chomp.downcase.to_sym
+  if @students.any? { |student| student[:cohort] == choice }
+    print(@students.select { |student| student[:cohort] === choice })
+  else
+    puts 'Sorry, there are no students for that month.'
+  end
 end
 
 def capitalise_words(words)
